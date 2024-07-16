6ec6e730a5fda3b5d0272f1d929de146e234f139
Omar Abdullah Mahran <109427483+Omar-A-Mahran9@users.noreply.github.com>
2024-07-16 18:07:52 +0300

    Your commit message

--- Desktop/webStdy Projects/Ghazal_Landing_page/nuxt.config.ts
+++ Desktop/webStdy Projects/Ghazal_Landing_page/nuxt.config.ts
@@ -2,29 +2,49 @@
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  build: {
-    transpile: ["vuetify"],
-  },
-  css: ["@/assets/style/main.css"],
-  modules: [
-    "@nuxtjs/i18n",
-    (_options, nuxt) => {
-      nuxt.hooks.hook("vite:extendConfig", (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }));
-      });
+    devtools: { enabled: true },
+    build: {
+        transpile: ["vuetify"],
     },
-  ],
+    css: ["@/assets/style/main.css"],
+    modules: [
+        "@nuxtjs/i18n",
+        (_options, nuxt) => {
+            nuxt.hooks.hook("vite:extendConfig", (config) => {
+                // @ts-expect-error
+                config.plugins.push(vuetify({ autoImport: true }));
+            });
+        },
+    ],
 
-  i18n: {
-    vueI18n: "./i18n.config.ts", // if you are using custom path, default
-  },
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
-      },
+    i18n: {
+        // lazy: true,
+        langDir: "locales",
+        strategy: "prefix_and_default",
+        detectBrowserLanguage: false,
+        locales: [
+            {
+                code: "en",
+                iso: "en",
+                dir: "ltr",
+                name: "english",
+                file: "en.json",
+            },
+            {
+                code: "ar",
+                iso: "ar",
+                dir: "rtl",
+                name: "عربي",
+                file: "ar.json",
+            },
+        ],
+        defaultLocale: "ar",
     },
-  },
+    vite: {
+        vue: {
+            template: {
+                transformAssetUrls,
+            },
+        },
+    },
 });
